e04953ed9f034b5bb15a84bfad04765f13c7e8cd
wcardoso <wesleysantoscardoso@outlook.com>
2017-06-01 00:48:31 -0300

    Update contato.blade.php

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -43,7 +43,7 @@
 	
 
 	<div class="col s12 m7 left">
-	<iframe src="https://www.google.com/maps/embed?pb=!1m17!1m11!1m3!1d12894.580228371278!2d-47.82273302633751!3d-15.795869267151144!2m2!1f0!2f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5cbd10b8335993c5!2sPal%C3%A1cio+da+Alvorada!5e1!3m2!1spt-BR!2sbr!4v1496087963495" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+	<iframe src="https://www.google.com/maps/embed?pb=!1m17!1m11!1m3!1d12894.580228371278!2d-47.82273302633751!3d-15.795869267151144!2m2!1f0!2f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5cbd10b8335993c5!2sPal%C3%A1cio+da+Alvorada!5e1!3m2!1spt-BR!2sbr!4v1496087963495" width="500" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 	</div>
 	</div>
 
